ec3a8412a77f8ad3fdf66d0883f6638f43aa0da9
Kaushik-sv <kaushiksv@infrrd.ai>
2020-11-26 22:47:04 +0530

    Greeting message dynamic

--- src/app/blogger/bookmark/bookmark.component.ts
+++ src/app/blogger/bookmark/bookmark.component.ts
@@ -9,6 +9,8 @@ import { DataStorageService } from 'src/app/shared/services/data-storage.service
 })
 export class BookmarkComponent implements OnInit {
  article:[]
+ hours=new Date().getHours();
+ greeting:string
 ​  url:string="https://1.bp.blogspot.com/-Sl-SXtgttF0/Xm3CcJit8TI/AAAAAAAAACE/6Qgzudb0wYs5quvLc2wXJQJ0BbWgslgrwCLcBGAsYHQ/s1600/1%2B%25281%2529.png"
 length:number;
   flag:boolean=false;
@@ -27,6 +29,23 @@ this.flag=true;
        this.length=this.article.length;
         
       })
+      if(this.hours>0 && this.hours < 4) 
+      {
+        this.greeting="Good Night"
+      }else if(this.hours>5 && this.hours < 11)
+      {
+        this.greeting="Good Morning"
+      
+      }else if(this.hours>12 && this.hours < 17)
+      {
+        this.greeting="Good Evening"
+      }
+      else
+      {
+        this.greeting="Good Night"
+      }
+    
+
   }
 ​onDelete(id:string)
 {
@@ -42,5 +61,9 @@ onClick(id)
   this.data.postId=id;
   this.router.navigate(['/read-post'])
 }
+onNewPost()
+{
 
+  this.router.navigate(['/new-post'])
+}
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -10,8 +10,25 @@ export class NavbarComponent implements OnInit {
   
 
   constructor(private router:Router) { }
+ greeting:string
+ hours=new Date().getHours();
 
   ngOnInit(): void {
+    if(this.hours>0 && this.hours < 4) 
+    {
+      this.greeting="Good Night"
+    }else if(this.hours>5 && this.hours < 11)
+    {
+      this.greeting="Good Morning"
+    
+    }else if(this.hours>12 && this.hours < 17)
+    {
+      this.greeting="Good Evening"
+    }
+    else
+    {
+      this.greeting="Good Night"
+    }
   
   }
 onClick()
